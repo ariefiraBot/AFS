1b2dad5366307dccb473a641a03ee852c977bce9
AriefBot <42173620+ariefiraBot@users.noreply.github.com>
2018-11-24 20:17:43 -0800

    Update 01.py

--- 01.py
+++ 01.py
@@ -11,7 +11,7 @@ from bs4 import BeautifulSoup
 from googletrans import Translator
 import youtube_dl
 #=============
-cl = LineClient() #authToken='EwRlAu3wso75maSj2fw1.aJEW7djVW2QrDd30OA0LKq.KEWZtYeQ7oUohRb1mZDb+tI7PAdF5O7EFZu2EB6Ol/o=')
+cl = LineClient() 
 cl.log("Auth Token : " + str(cl.authToken))
 channel = LineChannel(cl)
 cl.log("Channel Access Token : " + str(channel.channelAccessToken))
@@ -22,59 +22,59 @@ lineSettings = cl.getSettings()
 mid = cl.getProfile().mid
 responsename1 = cl.getProfile().displayName
 
-#ki = LineClient(authToken='EvLV2qK6lkJPBWBvOXI6.YFTivvUQlJGyQ0q4WC5f5G.W7Tvs5t4FOLfP6L3/nJNrEDbhXC0lnpP62zUqAfszr8=')
-#ki.log("Auth Token : " + str(ki.authToken))
-#channel1 = LineChannel(ki)
-#ki.log("Channel Access Token : " + str(channel1.channelAccessToken))
-#lineProfile = ki.getProfile()
-#lineSettings = ki.getSettings()
-#Amid = ki.getProfile().mid
-#responsename2 = ki.getProfile().displayName
+ki = LineClient()
+ki.log("Auth Token : " + str(ki.authToken))
+channel1 = LineChannel(ki)
+ki.log("Channel Access Token : " + str(channel1.channelAccessToken))
+lineProfile = ki.getProfile()
+lineSettings = ki.getSettings()
+Amid = ki.getProfile().mid
+responsename2 = ki.getProfile().displayName
 
-#kk = LineClient(authToken='Evgzt2YDGu5j8fI6S7y0.HekonXOId0LOrlrdV8suWa.Za9/BK3lzl/gXPpsb+8XV/NuwKNbdF6FWPfC2rPf9j0=')
-#kk.log("Auth Token : " + str(kk.authToken))
-#channel2 = LineChannel(kk)
-#kk.log("Channel Access Token : " + str(channel2.channelAccessToken))
-#lineProfile = kk.getProfile()
-#lineSettings = kk.getSettings()
-#Bmid = ki.getProfile().mid
-#responsename3 = ki.getProfile().displayName
+kk = LineClient()
+kk.log("Auth Token : " + str(kk.authToken))
+channel2 = LineChannel(kk)
+kk.log("Channel Access Token : " + str(channel2.channelAccessToken))
+lineProfile = kk.getProfile()
+lineSettings = kk.getSettings()
+Bmid = ki.getProfile().mid
+responsename3 = ki.getProfile().displayName
 
-#kc = LineClient(authToken='EvFUiD2pfNELRC3Aer45.As29tpM17+gWKq/m5Yz8Xq.WebKAPovwWTLwdjnqmglCD8iV+zDDoDIsP7eOSE3fhc=')
-#kc.log("Auth Token : " + str(kc.authToken))
-#channel3 = LineChannel(kc)
-#kc.log("Channel Access Token : " + str(channel3.channelAccessToken))
-#lineProfile = kc.getProfile()
-#lineSettings = kc.getSettings()
-#Cmid = kc.getProfile().mid
-#responsename4 = kc.getProfile().displayName
+kc = LineClient()
+kc.log("Auth Token : " + str(kc.authToken))
+channel3 = LineChannel(kc)
+kc.log("Channel Access Token : " + str(channel3.channelAccessToken))
+lineProfile = kc.getProfile()
+lineSettings = kc.getSettings()
+Cmid = kc.getProfile().mid
+responsename4 = kc.getProfile().displayName
 
-#km = LineClient(authToken='EwMTHv3xaSSvn60tes8c.qXiB6k3zJEG+VPAZ98trJa.AuMcEd6vA5w6wiZfyxlFAwSAu2o/Jh0eN9DGZIOEmoo=')
-#km.log("Auth Token : " + str(km.authToken))
-#channel4 = LineChannel(km)
-#km.log("Channel Access Token : " + str(channel4.channelAccessToken))
-#lineProfile = km.getProfile()
-#lineSettings = km.getSettings()
-#Dmid = km.getProfile().mid
-#responsename5 = km.getProfile().displayName
+km = LineClient()
+km.log("Auth Token : " + str(km.authToken))
+channel4 = LineChannel(km)
+km.log("Channel Access Token : " + str(channel4.channelAccessToken))
+lineProfile = km.getProfile()
+lineSettings = km.getSettings()
+Dmid = km.getProfile().mid
+responsename5 = km.getProfile().displayName
 
-#kb = LineClient(authToken='EwZD2TMWRC8DLQeUU9Z2.hmdPzUzp8pDxkggTRRJByG.TpUnBKD5WpzVyF1SLVgfHCRajdwHRf+PYj09TLQEj2s=')
-#kb.log("Auth Token : " + str(kb.authToken))
-#channel5 = LineChannel(kb)
-#kb.log("Channel Access Token : " + str(channel5.channelAccessToken))
-#lineProfile = kb.getProfile()
-#lineSettings = kb.getSettings()
-#Emid = kb.getProfile().mid
-#responsename6 = kb.getProfile().displayName
+kb = LineClient()
+kb.log("Auth Token : " + str(kb.authToken))
+channel5 = LineChannel(kb)
+kb.log("Channel Access Token : " + str(channel5.channelAccessToken))
+lineProfile = kb.getProfile()
+lineSettings = kb.getSettings()
+Emid = kb.getProfile().mid
+responsename6 = kb.getProfile().displayName
 
-#sw = LineClient(authToken='EvhD7rlI4FsSG4mjarXe.fi6AOZ9NS4Fsm3ey6B9rlG.IqporEXQK+RvUBPmwanuqAym2EBaTTbbN5tNNduoGIQ=')
-#sw.log("Auth Token : " + str(sw.authToken))
-#channel6 = LineChannel(sw)
-#sw.log("Channel Access Token : " + str(channel6.channelAccessToken))
-#lineProfile = sw.getProfile()
-#lineSettings = sw.getSettings()
-#Zmid = sw.getProfile().mid
-#responsename7 = sw.getProfile().displayName
+sw = LineClient()
+sw.log("Auth Token : " + str(sw.authToken))
+channel6 = LineChannel(sw)
+sw.log("Channel Access Token : " + str(channel6.channelAccessToken))
+lineProfile = sw.getProfile()
+lineSettings = sw.getSettings()
+Zmid = sw.getProfile().mid
+responsename7 = sw.getProfile().displayName
 
 print("---LOGIN SUCCES---")
 
@@ -83,9 +83,9 @@ owner = ["ue6d8f9ef8f820fad9c65bbb5d1ec714b"]
 admin = ["ue6d8f9ef8f820fad9c65bbb5d1ec714b"]
 staff = ["ue6d8f9ef8f820fad9c65bbb5d1ec714b"]
 
-KAC = [cl]
-ABC = [cl]
-Bots = [mid]
+KAC = [cl,ki,kk,kc,km,kb]
+ABC = [cl,ki,kk,kc,km,kb]
+Bots = [mid,Amid,Bmid,Cmid,Dmid,Zmid]
 Saints = admin + staff
 
 protectqr = []
